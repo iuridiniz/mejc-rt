93a4a46e9315af171bbe0f0725f22c4dddd6c5e5
Iuri Diniz <iuridiniz@gmail.com>
2015-12-08 11:02:11 -0300

    refactoring models

--- mejcrt/models.py
+++ mejcrt/models.py
@@ -8,13 +8,16 @@ from utils import onlynumbers, iconv, tokenize
 
 # Models
 blood_types = ('A+', 'B+', 'AB+', "O+", 'A-', 'B-', 'AB-', "O-")
+patient_types = ("RN", "G", "O")
+valid_locals = ["unidade-a", "unidade-b", 'unidade-b4', 'alto-risco',
+        'uti-neonatal', 'uti-materna', 'sem-registro']
 
 class Patient(ndb.Model):
     name = ndb.StringProperty(indexed=False, required=True)
     code = ndb.StringProperty(indexed=True, required=True, validator=onlynumbers)
     blood_type = ndb.StringProperty(indexed=False, required=True, choices=blood_types)
     type_ = ndb.StringProperty(indexed=False, required=True,
-        choices=["RN", "G", "O"])
+        choices=patient_types)
 
     name_tags = ndb.ComputedProperty(lambda self: self._gen_tokens_for_name(self.name), repeated=True)
     code_tags = ndb.ComputedProperty(lambda self: self._gen_tokens_for_code(self.code), repeated=True)
@@ -32,9 +35,9 @@ class Transfusion(ndb.Model):
     patient = ndb.StructuredProperty(Patient)
     nhh_code = ndb.StringProperty(indexed=True, required=True, validator=onlynumbers)
     date = ndb.DateProperty(indexed=True, required=True)
+
     local = ndb.StringProperty(indexed=False, required=True,
-        choices=["unidade-a", "unidade-b", 'unidade-b4', 'alto-risco',
-                 'uti-neonatal', 'uti-materna', 'sem-registro'])
+        choices=valid_locals)
     bags = ndb.LocalStructuredProperty(BloodBag, repeated=True)
     tags = ndb.StringProperty(repeated=True, indexed=False,
         choices=['rt', "ficha-preenchida", "carimbo-plantao",
