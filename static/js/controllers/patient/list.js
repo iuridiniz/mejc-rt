878924266e09f37284db86cac35f1efe3f184f6b
Iuri Diniz <iuridiniz@gmail.com>
2015-12-22 23:12:58 -0300

    search button for list patients is working

--- static/js/controllers/patient/list.js
+++ static/js/controllers/patient/list.js
@@ -9,21 +9,24 @@
             $location.path("/login");
             return;
         }
+        
+        ctrl.loading = true;
 
         ctrl.patients = [];
         ctrl.page = 1;
         ctrl.filter = "";
+        ctrl.applied_filter = null;
         ctrl.fields = "name,code";
         ctrl.offset = 0;
         ctrl.max = 20;
         
         ctrl.total = 0;
         ctrl.count = 0;
-        
 
         ctrl.prev_link = null;
         ctrl.next_link = null;
         ctrl.requestPatients = function(url) {
+            ctrl.loading = true;
             return $http.get(url).then(function(ret) {
                 var data = ret.data;
                 var patients = data.data;
@@ -38,23 +41,23 @@
                 if (data.offset + patients.length < data.count) {
                     ctrl.next_link = data.next;
                 }
-                
+                ctrl.offset = data.offset;
+                ctrl.page = Math.floor(data.offset / data.max) + 1;
                 ctrl.total = data.total;
                 ctrl.count = data.count;
+                ctrl.applied_filter = data.filter;
+                ctrl.loading = false;
             });
         };
         
         ctrl.goToNext = function() {
             if (ctrl.next_link) {
                 ctrl.requestPatients(ctrl.next_link);
-                ctrl.page += 1;
             }
         };
         ctrl.goToPrev = function() {
             if (ctrl.prev_link) {
-                ctrl.requestPatients(ctrl.prev_link).then(function() {
-                    ctrl.page -= 1;
-                });
+                ctrl.requestPatients(ctrl.prev_link);
             }
         };
         ctrl.buildUrl = function() {
@@ -65,6 +68,25 @@
                     fields: ctrl.fields,
             };
             return  "/api/v1/patient/?" + $httpParamSerializer(query)
+        };
+        
+        ctrl.searchKeyUp = function(ev) {
+            //console.log("SearchKeyUp", ev);
+            if (ev.keyCode == 13) {
+                ev.preventDefault();
+                ctrl.searchClick();
+            }
+        };
+        ctrl.clearSearchClick = function() {
+            console.log("clearSearchClick");
+            ctrl.filter = "";
+            ctrl.offset = 0;
+            ctrl.requestPatients(ctrl.buildUrl());
+        };
+        ctrl.searchClick = function() {
+            //console.log("searchClick");
+            ctrl.offset = 0;
+            ctrl.requestPatients(ctrl.buildUrl());
         };
         ctrl.requestPatients(ctrl.buildUrl());
         
